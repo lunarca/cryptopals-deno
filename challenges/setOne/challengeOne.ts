ba44c569702a9a58cbb7a9f3276e0ae410c631d8
Alex DeFreese <alexdefreese@gmail.com>
2022-04-29 08:38:57 -0400

    Initialize args and challenge 1-1

---
+++ challenges/setOne/challengeOne.ts
@@ -0,0 +1,14 @@
+export function challengeOne() {
+    console.log("Cryptopals challenge 1-1")
+
+    const hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    const b64String = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+    console.log("Challenge: Convert hex string to base 64")
+    console.log(`Hex String: ${hexString}`)
+    console.log(`Base 64 String: ${b64String}`)
+    console.log("Executing")
+
+
+    
+}
---
+++ main.ts
@@ -0,0 +1,26 @@
+import yargs from 'https://deno.land/x/yargs/deno.ts'
+import { Arguments } from 'https://deno.land/x/yargs/deno-types.ts'
+import { challengeOne } from "./challenges/setOne/challengeOne.ts";
+
+function main() {
+
+    parseArgs();
+
+}
+
+function parseArgs(): Arguments {
+    return yargs(Deno.args)
+        // Yargs says to use an Any type for this, so yikes I guess
+        // deno-lint-ignore no-explicit-any
+        .command("one", "Cryptopals Set One", (yargs: any) => 
+            yargs
+            .command("one", "Cryptopals Challenge 1-1", {}, challengeOne)
+            .demandCommand(1)
+        )
+        .help()
+        .strictCommands()
+        .demandCommand(2)
+        .parse()
+}
+
+main()
